0eb9c41331480567371bbb9d872ccfd08ea6b6ea
Gabriel Bauer <Gabweb@users.noreply.github.com>
2018-08-11 20:10:27 +0200

    feat: Favicon & small adjustments

---
+++ frontend/src/app/shared/components/logo-icon/logo-icon.component.ts
@@ -0,0 +1,13 @@
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-icon',
+  templateUrl: './logo-icon.component.html',
+  styleUrls: ['./logo-icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LogoIconComponent {
+
+  constructor() { }
+
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { BreakpointService } from '@shared/services/breakpoint/breakpoint.service';
 import { FooterComponent } from '@shared/components/footer/footer.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
@@ -7,16 +8,20 @@ import { TileDataService } from '@shared/services/tileData/tileData.service';
 import { ModalComponent } from '@shared/components/modal/modal.component';
 import { AuthService, Oauth2Service } from '@shared/services/auth';
 import { BackendService } from '@shared/services/backend';
+import { LogoIconComponent } from './components/logo-icon/logo-icon.component';
+
 
 const components = [
   FooterComponent,
   HeaderComponent,
-  ModalComponent
+  ModalComponent,
+  LogoIconComponent
 ];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [components],
   exports: [components],
